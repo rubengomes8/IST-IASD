e65145a141ec97082a1d84d6541ef0db27d37e57
JohnM <mfsmm.98@gmail.com>
2019-10-20 14:47:31 +0100

    extensive code implementation

--- solution.py
+++ solution.py
@@ -7,83 +7,94 @@ class ASARProblem(Problem):
 
     airport = {}
     aircraft = {}
-    L = {}
     fleet = []
 
-    def __init__(self, initial, goal, graph):
-        Problem.__init__(self, initial, goal)
-        self.graph = graph
+    def __init__(self):
+        Problem.__init__(self, None, None)
 
     def actions(self, state):
-        """ Return the actions that can be executed in the given state."""
-        return list(self.graph.get(state).keys())
+        """ Return the actions that can be executed in the given state.
+            This should return a set of possible actions for each aircraft.
+            An action in this context is defined by a dictionary with actions for each aircraft"""
+        action = set()
+        if state.aircraft_status is None:
+            # Initial State
+            # TODO:cobinatoria fdd nao me apetece pensar
+            raise NotImplementedError
+
+        else:
+            # Not Initial State
+            for aircraft, status in state.aircraft_status.items():
+                raise NotImplementedError
+
+        return action
 
     def result(self, state, action):
         """Given state and action, return a new state that is the result of the action.
             Action is assumed to be a valid action in the state """
-        return tuple(new_state)
+        raise NotImplementedError
 
     def goal_test(self, state):
         """Return True if the state is a goal. The default method compares the
             state to self.goal or checks for state in self.goal if it is a
             list, as specified in the constructor. Override this method if
             checking against a single self.goal is not enough."""
-        return state == self.goal
+
+        # There are still legs left to fly
+        if len(state.remaining_legs) > 0:
+            return False
+
+        # Checks if aircraft base airport is the same as the current airport
+        for aircraft in state.aircraft_status.values():
+            if aircraft['base'] != aircraft['cur']:
+                return False
+
+        return True
 
     def path_cost(self, cost_so_far, state1, action, state2):
-        return cost_so_far + (self.graph.get(state1, state2) or infinity)
+        raise NotImplementedError
 
     def value(self, state):
         """ Given state and action, return a new state that is the result of the action.
         Action is assumed to be a valid action in the state """
         raise NotImplementedError
 
-    def heuristic(self, node):
+    def heuristic(self, state):
         """h function is straight-line distance from a node's state to goal."""
-        locs = getattr(self.graph, 'locations', None)
-        if locs:
-            if type(node) is str:
-                return int(distance(locs[node], locs[self.goal]))
+        raise NotImplementedError
 
-            return int(distance(locs[node.state], locs[self.goal]))
-        else:
-            return infinity
-
-    def load(self,f):
+    def load(self, f):
         """Loads a problem from file f"""
+        leg_counter = 0
         for ln in (ln for ln in f.readlines() if len(ln.split()) > 0):
-            l = ln.split()
-
+            l_array = ln.split()
             # Inserts in airport dict curfews
-            if l[0] == 'A':
-                self.airport[l[1]] = (l[2],l[3])
+            if l_array[0] == 'A':
+                self.airport[l_array[1]] = Airport(hour_to_min(l_array[2]), hour_to_min(l_array[3]))
 
             # Inserts in aircraft dict rotation times
-            elif l[0] == 'C':
-                self.aircraft[l[1]] = l[2]
+            elif l_array[0] == 'C':
+                self.aircraft[l_array[1]] = l_array[2]
 
             # Inserts a plane in the fleet (reg, class)
-            elif l[0] == 'P':
-                self.fleet.append((l[1],l[2]))
+            elif l_array[0] == 'P':
+                self.fleet.append((l_array[1], l_array[2]))
 
             # Insert a leg
-            elif l[0] == 'L':
-                class_prof = []
-                #self.L[(l[1], l[2])] = l[3],l[4]
-                for i in range(4, len(l)-1, 2):
-                    class_prof.append(l[i])
-                    class_prof.append(l[i+1])
-                self.L[(l[1], l[2])]= l[3],class_prof
-                print(self.L)
-
-
-                ## TODO: each aircraft class has different distance cost. Create graph for each class?
-                #self.graph.connect(l[1],l[2],distance)
+            elif l_array[0] == 'L':
+                leg_counter += 1
+                profit = {}
+                for i in range(4, len(l_array)-1, 2):
+                    profit[l_array[i]] = int(l_array[i+1])
+                self.airport[l_array[1]] = Leg(l_array[2], hour_to_min(l_array[3]), profit)
 
             else:
                 raise RuntimeError("Bad Format Error")
 
-    def save(self,f, state):
+        # Construct the Problem
+        self.initial = State(None, leg_counter)
+
+    def save(self, f, state):
         """saves a solution state s to file f"""
 
         # No solution was found
@@ -91,34 +102,67 @@ class ASARProblem(Problem):
             f.write("Infeasible")
             return
 
-class Time:
 
-    def min_to_hour(self, min):
-        return str(min/60 + min%60)
+class State:
 
-    def hour_to_min(self, hours): #hours in string
-        hour = int(hours[0:1])
-        minutes = int(hours[2:3])
-        return hour*60+minutes
+    def __init__(self, aircraft_status, remaining_legs):
+
+        self.aircraft_status = aircraft_status
+        self.remaining_legs = remaining_legs
+
+
+# Class containing information on an Airport
+class Airport:
+
+    def __init__(self, open_time, close_time):
+        self.open = open_time
+        self.close = close_time
+        self.legs = []
+
+    def add_leg_to_airport(self, leg):
+
+        self.legs.append(leg)
+
+
+# Class containing information on a leg.
+class Leg:
+
+    def __init__(self, arr, flight_time, profit):
+        self.arr = arr
+        self.flight_time = flight_time
+        self.profit = profit
+
+    # Returns arrival airport, time of arrival and profit of given leg
+    def calculate_route(self, a_class, std):
+
+        return self.arr, std+self.flight_time, self.profit[a_class]
+
+
+# Auxiliary Time functions
+
+def min_to_hour(minute):
+    return str(minute/60 + minute % 60)
+
+
+def hour_to_min(hours):  # hours in string
+    hour = int(hours[0:1])
+    minutes = int(hours[2:3])
+    return hour*60+minutes
+
 
 def main():
 
     if len(sys.argv) > 1:
 
-
-        asar = ASARProblem(None, None, None)
+        asar = ASARProblem()
 
         with open(sys.argv[1]) as f:
             asar.load(f)
             f.close()
 
-
-
     else:
         print("Usage:", sys.argv[0], "<filename>")
 
 
-
 if __name__ == '__main__':
     main()
-
